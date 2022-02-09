2335cdd7d07333360bbd649e1870f5418d7ea2e6
victor <victordj587@gmail.com>
2022-02-09 16:48:16 +0100

    update

--- 2dRaymarching/sketch.js
+++ 2dRaymarching/sketch.js
@@ -2,6 +2,8 @@ var obstacles = [];
 
 function setup() {
   createCanvas(600,600);
+  let o = new obstacle(300,300,100)
+  obstacles.push(o);
 }
 
 function draw() {
@@ -16,7 +18,7 @@ class obstacle {
   }
   show() {
     for(var i = 0 ; i < obstacles.length ; i++ ) {
-      
+      circle(obstacles[i].x, obstacles[i].y, obstacles[i].r*2 )
     }
   }
 }
